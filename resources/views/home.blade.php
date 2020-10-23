7f2b91d971749cfa747e5226566a37ce7df21ce9
GUSTAVO GRANDISOLLI ZWICKER <gustavo.zwicker@etec.sp.gov.br>
2020-10-23 16:42:22 -0300

    Primeiro commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Vestibulando</title>
+        <meta charset="utf-8">   
+        <link href="/css/index.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id=tela>
+            <div id=headerph>
+            <header id=cabecalho>
+                <img  src="/img/logo.png">
+                <nav id=menu>
+                    <div class=links>
+                        <a href= "/welcome"><c>Home</c></a>
+                        <a href= "/welcome">Universidades</a>
+                        <a href= "/welcome">Redações</a>
+                        <a href= "/welcome">Quem Somos</a>
+                        <a href= "/welcome">Cadastro/Login</a>
+                        <input type="text" placeholder="Pesquise no Vestibulando">
+                        <input type="submit" value="">
+
+                    </div>
+                </nav>
+            </header>
+            </div>
+            <div id=content>
+                <div id=sslider>
+                    <div id=sliderbg>
+                    <a class=prev> <img src="/img/seta.png"> </a>
+                    <div id=slider>
+                        <img src="/img/img1.png" width=calc(1600/(1006/360))px height=360px>
+                    </div>
+                    <a class=next> <img src="/img/seta.png"> </a>
+                    </div>
+                </div>
+            </div>
+            <div id=colunas>
+                <div class=col1>
+                </div>
+                <div class=col2>
+                </div>
+                <div class=col3>
+                </div>
+            </div>
+            <div id=rodape>
+            <div class="row">
+  <div class="column"><div class=rodapeinfo><img src="/img/logo_r.png">
+                    <br>Contato: gustavo.zwicker@etec.sp.gov.br</br></div></div>
+  <div class="column"><div class=rodapeinfo2>Copyright (c) - 2020 Vestibulando Informação e Vestibulares
+                    <p>Todos os direitos reservados</p></div></div>
+            </div>
+                </div>
+            </div>
+            <b></b>
+        </div>
+    </body>
+</html>
