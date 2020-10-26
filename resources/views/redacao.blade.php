3da7ce4a1f48d2548ee8128bc0d27a074925e274
LUCAS VICENTE PRADO <lucas.prado59@etec.sp.gov.br>
2020-10-26 20:01:57 -0300

    lucas

---
+++ resources/views/redacao.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Vestibulando</title>
+        <meta charset="utf-8">   
+        <link href="/css/index.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id=tela>
+            <div id=headerph>
+            <header id=cabecalho>
+                <img  src="/img/logo.png">
+                <nav id=menu>
+                    <div class=links>
+                        <a href= "/welcome"><c>Home</c></a>
+                        <a href= "/welcome">Universidades</a>
+                        <a href= "/welcome">Redações</a>
+                        <a href= "/welcome">Quem Somos</a>
+                        <a href= "/welcome">Cadastro/Login</a>
+                        <input type="text" placeholder="Pesquise no Vestibulando">
+                        <input type="submit" value="">
+
+                    </div>
+                </nav>
+            </header>
+            </div>
+            
+            <div id=rodape>
+            <div class="row">
+  <div class="column"><div class=rodapeinfo><img src="/img/logo_r.png">
+                    <br>Contato: gustavo.zwicker@etec.sp.gov.br</br></div></div>
+  <div class="column"><div class=rodapeinfo2>Copyright (c) - 2020 Vestibulando Informação e Vestibulares
+                    <p>Todos os direitos reservados</p></div></div>
+            </div>
+                </div>
+            </div>
+            <b></b>
+        </div>
+    </body>
+</html>
