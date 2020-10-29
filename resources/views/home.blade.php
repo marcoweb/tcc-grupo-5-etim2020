8cc2e4423142cff3dcc9578f62763e61ff225836
GUSTAVO GRANDISOLLI ZWICKER <gustavo.zwicker@etec.sp.gov.br>
2020-10-28 21:22:06 -0300

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,41 +24,54 @@
                 </nav>
             </header>
             </div>
-            <main id=content>
+                <main id=content>
                     <div id=sliderbg>
-                    <a class=prev> <img src="/img/seta.png"> </a>
-                    <div id=slider>
-                        <ul class=slider>
-                        <li>
-                        <input type="radio" id="slide1" name="slide" checked>
-                        <label for="slide1"></label>
-                        <img src="/img/img1.png" width=calc(100%/(1006/360))px height=360px>
-                        </li>
-                        <li>
-                        <input type="radio" id="slide2" name="slide">
-                        <label for="slide2"></label>
-                        <img src="/img/img2.png" width=calc(100%/(1024/360))px height=360px>
-                        </li>
-                        <li>
-                        <input type="radio" id="slide3" name="slide">
-                        <label for="slide3"></label>
-                        <img src="/img/img3.png" width=calc(100%/(1006/360))px height=360px>
-                        </li>
-                        </ul>
+                        <a class=prev> <img src="/img/seta.png"> </a>
+                            <div id=slider>
+                                <ul class=slider>
+                                    <li>
+                                        <input type="radio" id="slide1" name="slide" checked>
+                                        <label for="slide1"></label>
+                                        <img src="/img/img1.png" width=calc(100%/(1006/360))px height=360px>
+                                    </li>
+                                    <li>
+                                        <input type="radio" id="slide2" name="slide">
+                                        <label for="slide2"></label>
+                                        <img src="/img/img2.png" width=calc(100%/(1024/360))px height=360px>
+                                    </li>
+                                    <li>
+                                        <input type="radio" id="slide3" name="slide">
+                                        <label for="slide3"></label>
+                                        <img src="/img/img3.png" width=calc(100%/(1006/360))px height=360px>
+                                    </li>
+                                </ul>
+                            </div>
+                        <a class=next><img src="/img/seta.png"> </a>
                     </div>
-                    <a class=next><img src="/img/seta.png"> </a>
+                    <div id=row>
+                        <div id=colu1>
+                            <img class=coluimg src="/img/unesplogo.png">
+                            <h1>Universidade Estadual <br>Paulista</br></h1>
+                            <h2><a href= "/welcome">Mais informações</a><h2>
+                        </div>
+                        <div id=colu2>
+                            <img class=coluimg src="/img/unesplogo.png">
+                            <h1>Universidade Estadual Paulista</h1>
+                            <h2><a href= "/welcome">Mais informações</a><h2>
+                        </div>
+                        <div id=colu3>
+                            <img class=coluimg src="/img/unesplogo.png">
+                            <h1 class=shit>Universidade Estadual Paulista</h1>
+                            <h2><a href= "/welcome">Mais informações</a><h2>
+                        </div>
                     </div>
-            </div>
-            <div id=colunas>
-                
+                </main>
             </div>
             <footer id=rodape>
-            <div class="row">
   <div class="column"><div class=rodapeinfo><img src="/img/logo_r.png">
                     <br>Contato: gustavo.zwicker@etec.sp.gov.br</br></div></div>
   <div class="column"><div class=rodapeinfo2>Copyright (c) - 2020 Vestibulando Informação e Vestibulares
                     <p>Todos os direitos reservados</p></div></div>
-            </div>
 </footer>
     </main>
         </div>
